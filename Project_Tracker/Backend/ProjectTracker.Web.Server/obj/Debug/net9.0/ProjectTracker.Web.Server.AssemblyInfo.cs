8eb90adbdd2115d7784a3e6758244b53087bc4ba
Learjay <learjaylencina@iskolarngbayan.pup.edu.ph>
2026-03-01 22:30:56 -0500

    com

--- Project_Tracker/Backend/ProjectTracker.Web.Server/obj/Debug/net9.0/ProjectTracker.Web.Server.AssemblyInfo.cs
+++ Project_Tracker/Backend/ProjectTracker.Web.Server/obj/Debug/net9.0/ProjectTracker.Web.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTracker.Web.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c75e507036be661ed21b61fe2bd9aa7202a1b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfe31d7223b197721cbb4ed2a922a93ab2f47b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTracker.Web.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTracker.Web.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
